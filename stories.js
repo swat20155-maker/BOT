a0fc10d936868fdaac348907153b39e95c97a4c5
swat20155-maker <swat20155@gmail.com>
2026-04-03 15:12:41 +0300

    Update stories.js

--- stories.js
+++ stories.js
@@ -92,7 +92,7 @@ function createDashboardStoryElement(story, index) {
     }
         </button>
 
-        <button class="story-action-btn delete" onclick="deleteStory('${story.id}')">
+        <button class="story-action-btn delete" onclick="deleteStory('${story._id || story.id}')">
           <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align:-1px"><polyline points="3 6 5 6 21 6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg> Delete
         </button>
       </div>
